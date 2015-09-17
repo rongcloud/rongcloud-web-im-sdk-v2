46f04dfd3b8522c51bc804d930a187159a6e798c
yuhongda <admin123>
2015-09-17 18:33:07 +0800

    yuhongda: 整理实现思路，重新实现

--- src/internal/transportation/xhrpolling.ts
+++ src/internal/transportation/xhrpolling.ts
@@ -1,13 +1,19 @@
 module RongIMLib {
     export class XHRPolling implements Transportation {
         url: string;
+        connectStatus: string;
+        polling: any;
+        allowWithCrendentials: boolean;
+        isXHR: boolean = true;
+        empty: Function = new Function;
         //构造函数传入参数
         constructor(url: string) {
             this.url = url;
         }
         //创建通道
         createTransport(): any {
-
+            var request = this.createHtppRequest();
+            this.polling=request;
         }
         //传送消息流
         send(data: any): any {
@@ -19,7 +25,7 @@ module RongIMLib {
         }
         //处理通道关闭操作
         onClose(): any {
-
+            this.connectStatus = "close";
         }
         //通道异常操作
         onError(error: any): void {
@@ -29,15 +35,25 @@ module RongIMLib {
         addEvent(): void {
 
         }
-        //创建request对象，判断是否支持XMLHttpRequest or XdoMainRequest
-        createResueat(): any {
-            if ('XDomainRequest' in window)
-                return new window["XDomainRequest"]();
-            if ('XMLHttpRequest' in window && this.checkWithCredentials())
+        /***
+        *返回XMLHttpRequest或者XdomainRequest
+        *设置参数 isXHR、allowWithCrendentials
+        */
+        createHtppRequest(): any {
+            var hasCORS = typeof XMLHttpRequest !== 'undefined' && 'withCredentials' in new XMLHttpRequest(), self = this;
+            if ('undefined' != typeof XMLHttpRequest && hasCORS) {
+                self.allowWithCrendentials=true;
+                //isXHR 此处无需设置，默认true
                 return new XMLHttpRequest();
+            } else if ('undefined' != typeof XDomainRequest) {
+                self.isXHR = false;
+                return new XDomainRequest();
+            } else {
+                return new Function;
+            }
         }
         checkWithCredentials(): boolean {
-            if (!('XMLHttpRequest' in window))return false;
+            if (!('XMLHttpRequest' in window)) return false;
             var xmlRequest = new XMLHttpRequest();
             return xmlRequest.withCredentials !== undefined;
         }
