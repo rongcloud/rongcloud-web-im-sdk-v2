a7382d0ec7b5c4e51ced61d146fecfc19afbd375
yuhongda <yuhongda@rongcloud.cn>
2019-09-16 16:41:22 +0800

    fix: 连接状态非空判断

--- src/internal/connect_client.ts
+++ src/internal/connect_client.ts
@@ -192,7 +192,7 @@ module RongIMLib {
             var hasEvent = (typeof StatusEvent == "object");
             var me = this;
             me.socket.on("StatusChanged", function (code: any) {
-                if (me !== RongIMLib.Bridge._client.channel) {
+                if (RongIMLib.Bridge._client && me !== RongIMLib.Bridge._client.channel) {
                     return;
                 }
                 if (!hasEvent) {
